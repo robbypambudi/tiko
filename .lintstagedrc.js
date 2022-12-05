09c3d86fcd2889580cdd8d4c49be23dd7f763875
Robby Pambudi <5025211042@mhs.its.ac.id>
2022-12-06 04:17:21 +0700

    refactor: change folder structure and setup starter

---
+++ .lintstagedrc.js
@@ -0,0 +1,16 @@
+module.exports = {
+  // Type check TypeScript files
+  '**/*.(ts|tsx)': () => 'yarn tsc --noEmit',
+
+  // Lint & Prettify TS and JS files
+  '**/*.(ts|tsx|js)': (filenames) => [
+    `yarn eslint ${filenames.join(' ')}`,
+    `yarn prettier --write ${filenames.join(' ')}`,
+  ],
+
+  // Prettify only Markdown and JSON files
+  '**/*.(md|json)': (filenames) => `yarn prettier --write ${filenames.join(' ')}`,
+
+  // Minify the images
+  '*.{png,jpeg,jpg,gif,svg}': 'imagemin-lint-staged',
+};
